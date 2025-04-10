8cbc0a84b68c9520884157e00e799fc5cd630989
Rosalie <rosalie.nicaud@gmail.com>
2025-04-10 15:12:41 +0200

    config eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// import { defineConfig } from "eslint/config";
+// import js from "@eslint/js";
+// import globals from "globals";
+// import pluginReact from "eslint-plugin-react";
+
+
+// export default defineConfig([
+//   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+//   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.node } },
+//   pluginReact.configs.flat.recommended,
+// ]);
+
+import js from '@eslint/js'
+import globals from 'globals'
+import pluginReact from 'eslint-plugin-react'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*-(js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  } ,
+  pluginReact.configs.flat.recommended,
+]
